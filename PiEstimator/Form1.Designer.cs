8120dc7d060fa5a86d8bddf1d6afe403a37fec24
agrapes22 <agrapes22@gmail.com>
2021-05-21 19:41:20 -0700

    Initial Commit

--- PiEstimator/Form1.Designer.cs
+++ PiEstimator/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.answerText = new System.Windows.Forms.Label();
+            this.errorText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2512F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter number of terms:";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(212, 47);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(44, 113);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 2;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Approximate value";
+            // 
+            // answerText
+            // 
+            this.answerText.AutoSize = true;
+            this.answerText.Location = new System.Drawing.Point(41, 245);
+            this.answerText.Name = "answerText";
+            this.answerText.Size = new System.Drawing.Size(55, 13);
+            this.answerText.TabIndex = 4;
+            this.answerText.Text = "= 3.14159";
+            this.answerText.Visible = false;
+            // 
+            // errorText
+            // 
+            this.errorText.AutoSize = true;
+            this.errorText.ForeColor = System.Drawing.Color.Red;
+            this.errorText.Location = new System.Drawing.Point(172, 84);
+            this.errorText.Name = "errorText";
+            this.errorText.Size = new System.Drawing.Size(178, 13);
+            this.errorText.TabIndex = 5;
+            this.errorText.Text = "Please enter a valid number of terms";
+            this.errorText.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(451, 368);
+            this.Controls.Add(this.errorText);
+            this.Controls.Add(this.answerText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Approximate Pi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label answerText;
+        private System.Windows.Forms.Label errorText;
     }
 }
 
--- PiEstimator/Form1.cs
+++ PiEstimator/Form1.cs
@@ -16,5 +16,44 @@ namespace PiEstimator
         {
             InitializeComponent();
         }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            //make error text invisible from last click
+            errorText.Visible = false;
+
+            if (!String.IsNullOrWhiteSpace(inputBox.Text))
+            {
+                int inNum = 0;
+
+                if(int.TryParse(inputBox.Text, out inNum) == true)
+                {
+                    //set up answer variable and multiplier
+                    double answer = 0;
+                    double mult = 1.0;
+
+                    for(double i = 0; i <= inNum; i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            //add calculation to answer total
+                            answer = answer + ((4.0 * mult) / i);
+                            //change multiplier to get correct plus or minus
+                            mult = mult * (- 1.0);
+                        }
+                    }
+
+                    //display and show answerText
+                    answerText.Visible = true;
+                    answerText.Text = "=" + answer.ToString();
+                }
+                else
+                {
+                    //show errorText and hide answerText if input isn't a number
+                    errorText.Visible = true;
+                    answerText.Visible = false;
+                }
+            }
+        }
     }
 }
